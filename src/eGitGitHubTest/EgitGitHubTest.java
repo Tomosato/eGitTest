86177feb2c2581ba40c3d62a37f4c7ec4d5a8a29
Tomosato <t_shimamura@dreamarts.co.jp>
2014-07-02 23:41:08 +0900

    最初のコミット(EGit)

---
+++ src/eGitGitHubTest/EgitGitHubTest.java
@@ -0,0 +1,11 @@
+package eGitGitHubTest;
+
+public class EgitGitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Eclipseからこんにちは");
+	}
+
+}
